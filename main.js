e6fe05beb99331f5216fa5a11ee9ff1932e45e75
Aria Velasquez <aria.velasquez18@journalism.cuny.edu>
2022-03-18 21:31:31 -0400

    added images

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
 const select = document.querySelector(".filter");
 
 select.addEventListener("change", function (e) {
-    const chosen_food = e.target.value;
+    const chosen_justices = e.target.value;
     const currently_visible = document.querySelectorAll(".item.active");
-    const make_visible = document.querySelectorAll(`.item.${chosen_food}`);
+    const make_visible = document.querySelectorAll(`.item.${chosen_justices}`);
 
-    currently_visible.forEach(function (food) {
-        food.classList.remove("active");
+    currently_visible.forEach(function (justices) {
+        justices.classList.remove("active");
     });
 
-    make_visible.forEach(function (food) {
-        food.classList.add("active");
+    make_visible.forEach(function (justices) {
+        justices.classList.add("active");
     });
 });
