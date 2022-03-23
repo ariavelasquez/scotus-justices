1ed198658d414e4c8397c80f39013a6f9b73f850
Aria Velasquez <aria.velasquez18@journalism.cuny.edu>
2022-03-23 12:01:12 -0400

    html classes, css background color, event listener

--- main.js
+++ main.js
@@ -1,15 +1,17 @@
-const select = document.querySelector(".filter");
+const select = document.querySelectorAll(".filter");
 
-select.addEventListener("change", function (e) {
-    const chosen_justices = e.target.value;
-    const currently_visible = document.querySelectorAll(".item.active");
-    const make_visible = document.querySelectorAll(`.item.${chosen_justices}`);
-
-    currently_visible.forEach(function (justices) {
-        justices.classList.remove("active");
-    });
-
-    make_visible.forEach(function (justices) {
-        justices.classList.add("active");
+select.forEach (function (scotus) {
+    scotus.addEventListener("change", function (e) {
+        const chosen_justices = e.target.value;
+        const currently_visible = document.querySelectorAll(".item.active");
+        const make_visible = document.querySelectorAll(`.${chosen_justices}`);
+    
+        currently_visible.forEach(function (justices) {
+            justices.classList.remove("active");
+        });
+    
+        make_visible.forEach(function (justices) {
+            justices.classList.add("active");
+        });
     });
 });
